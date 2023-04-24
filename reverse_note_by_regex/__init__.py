666e8d4c823f226cc0984623496f875a931be491
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2023-04-24 21:49:25 +0200

    Show tooltip

--- reverse_note_by_regex/__init__.py
+++ reverse_note_by_regex/__init__.py
@@ -2,6 +2,7 @@
 
 from anki.notes import Note
 from aqt import mw, gui_hooks
+from aqt.utils import tooltip
 
 __name__ = 'reverse_note_by_regex'
 mappingsKey = 'mappings'
@@ -14,6 +15,7 @@ def change_to_reverse(note: Note) -> None:
     if any(re.search(regex, field) for field in note.fields for regex in config[regexKey]):
         note.mid = mw.col.models.id_for_name(config[noteTypeKey])
         mw.col.update_note(note)
+        tooltip("Note type changed to reverse")
         return
 
 
