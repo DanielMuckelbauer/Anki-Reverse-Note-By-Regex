71c452c7e89f7c91e23a5b5bf1cad6d552403d5e
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-02-21 07:03:50 +0100

    Show alert instead of tooltip

--- reverse_note_by_regex/__init__.py
+++ reverse_note_by_regex/__init__.py
@@ -2,20 +2,21 @@
 
 from anki.notes import Note
 from aqt import mw, gui_hooks
-from aqt.utils import tooltip
+from aqt.utils import showInfo
 
-__name__ = 'reverse_note_by_regex'
+# addonId = 'reverse_note_by_regex' -> for local development
+addonId = '197088151'
 mappingsKey = 'mappings'
 regexKey = 'regexes'
 noteTypeKey = 'noteType'
 
 
 def change_to_reverse(note: Note) -> None:
-    config = mw.addonManager.getConfig(__name__)
+    config = mw.addonManager.getConfig(addonId)
     if any(re.search(regex, field) for field in note.fields for regex in config[regexKey]):
         note.mid = mw.col.models.id_for_name(config[noteTypeKey])
         mw.col.update_note(note)
-        tooltip("Note type changed to reverse")
+        showInfo("Note type changed to reverse")
         return
 
 
