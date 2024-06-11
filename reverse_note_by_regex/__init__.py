6e3e04ddf1470a322d10a78a930a8bcf0802319c
Daniel Muckelbauer <daniel.muckelbauer@covermesh.com>
2024-06-11 09:43:22 +0200

    Ask before converting

--- reverse_note_by_regex/__init__.py
+++ reverse_note_by_regex/__init__.py
@@ -1,10 +1,9 @@
 ﻿import re
-
 from anki.notes import Note
 from aqt import mw, gui_hooks
-from aqt.utils import showInfo
+from aqt.utils import showInfo, askUser
 
-# addonId = 'reverse_note_by_regex' -> for local development
+# addonId = 'reverse_note_by_regex'  # -> for local development
 addonId = '197088151'
 mappingsKey = 'mappings'
 regexKey = 'regexes'
@@ -14,10 +13,12 @@ noteTypeKey = 'noteType'
 def change_to_reverse(note: Note) -> None:
     config = mw.addonManager.getConfig(addonId)
     if any(re.search(regex, field) for field in note.fields for regex in config[regexKey]):
-        note.mid = mw.col.models.id_for_name(config[noteTypeKey])
-        mw.col.update_note(note)
-        showInfo("Note type changed to reverse")
-        return
+        if askUser("Should this note be converted to reverse?"):
+            note.mid = mw.col.models.id_for_name(config[noteTypeKey])
+            mw.col.update_note(note)
+        else:
+            return
 
 
 gui_hooks.add_cards_did_add_note.append(change_to_reverse)
+
